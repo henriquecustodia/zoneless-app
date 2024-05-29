e343443ab8be410c618dfc3a67ff03f89e11fe9f
Henrique Custódia <martinshenrique8@gmail.com>
2024-05-29 09:57:14 -0300

    feat: add zoneless app example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
-import { Component, signal } from "@angular/core";
+import { AsyncPipe } from "@angular/common";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { timer } from "rxjs";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` {{ title() }} `,
+  imports: [],
+  template: ` 
+    {{ title() }} 
+  `,
   styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = signal("zoneless-app");
 
   constructor() {
     timer(3000).subscribe(() => {
-      this.title.set("new title");
+      this.title.set("new title after 3s");
     });
   }
 }
